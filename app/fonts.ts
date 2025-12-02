65dcce496340edb09157ca1a7c95d5381f70ac42
rachet-khanal <rachet.khanal@gmail.com>
2025-12-03 04:47:26 +0800

    minor: add required fonts

---
+++ app/fonts.ts
@@ -0,0 +1,31 @@
+import localFont from "next/font/local";
+
+export const bagoss = localFont({
+  src: "../public/fonts/BagossStandard-TRIAL-Regular.otf",
+  variable: "--font-bagoss",
+  display: "swap",
+});
+
+export const sohne = localFont({
+  src: "../public/fonts/Sohne-Buch.otf",
+  variable: "--font-sohne",
+  display: "swap",
+});
+
+export const agrandir = localFont({
+  src: "../public/fonts/PPAgrandir-Regular.woff2",
+  variable: "--font-agrandir",
+  display: "swap",
+});
+
+export const sfProDisplay = localFont({
+  src: "../public/fonts/SFPRODISPLAYMEDIUM.otf",
+  variable: "--font-sf-pro-display",
+  display: "swap",
+});
+
+export const sfCompactDisplay = localFont({
+  src: "../public/fonts/SF-Compact-Display-Medium.otf",
+  variable: "--font-sf-compact-display",
+  display: "swap",
+});
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { bagoss, sohne, agrandir, sfProDisplay, sfCompactDisplay } from "./fonts";
 
 export const metadata: Metadata = {
   title: "Juicebox",
@@ -13,7 +14,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={`${bagoss.variable} ${sohne.variable} ${agrandir.variable} ${sfProDisplay.variable} ${sfCompactDisplay.variable}`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
