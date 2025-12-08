c261d6f3246f7e978d9e9fe83daf4a8da2a5843d
rachet-khanal <rachet.khanal@gmail.com>
2025-12-08 14:48:36 +0800

    test: standalone route

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,24 +1,24 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from "next"
 
-export const dynamic = 'force-static'
+export const dynamic = "force-static"
 
 export default function manifest(): MetadataRoute.Manifest {
-  const repoName = process.env.NEXT_PUBLIC_GITHUB_REPO_NAME
-  const basePath = repoName ? `/${repoName}` : ""
+  // const repoName = process.env.NEXT_PUBLIC_GITHUB_REPO_NAME
+  // const basePath = repoName ? `/${repoName}` : ""
 
   return {
-    name: 'Juicebox',
-    short_name: 'Juicebox',
-    description: 'Juicebox App',
-    start_url: basePath || '/',
-    display: 'standalone',
-    background_color: '#0C0D10',
-    theme_color: '#0C0D10',
+    name: "Juicebox",
+    short_name: "Juicebox",
+    description: "Juicebox App",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#0C0D10",
+    theme_color: "#0C0D10",
     icons: [
       {
-        src: `${basePath}/images/logo.svg`,
-        sizes: 'any',
-        type: 'image/svg+xml',
+        src: `/images/logo.svg`,
+        sizes: "any",
+        type: "image/svg+xml",
       },
     ],
   }
